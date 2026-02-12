5dcbec0173fcede075241ef97b6c9041441502db
aan-nahdi <aan_nahdi@hotmail.com>
2026-02-13 02:31:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const sections=document.querySelectorAll('.section');
+
+function reveal(){
+sections.forEach((el,i)=>{
+const rect=el.getBoundingClientRect();
+
+if(rect.top < window.innerHeight-120){
+setTimeout(()=>{
+el.style.opacity="1";
+el.style.transform="translateY(0)";
+},i*150);
+}
+});
+}
+
+window.addEventListener('scroll',reveal);
+reveal();
